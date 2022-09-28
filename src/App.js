276569a4f012f2e347a222b62037bb385378ffb0
creativerze <creativerze@gmail.com>
2022-09-28 20:51:31 +0600

    added componests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <h2>Strength and Fitness Palace</h2>
+      <Home></Home>
     </div>
   );
 }
